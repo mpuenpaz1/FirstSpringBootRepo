70b9a04edbd98a58d9945388fe85bda8dd884f0f
marcospuente <marcospuente24@gmail.com>
2024-07-23 13:02:56 +0200

    update structure.

---
+++ 01-spring-boot-overview/02-dev-tools-demo/FirstSpringBoot-02/src/main/java/com/puente/springboot/demo/firstProjectSpringboot/rest/FunRestController.java
@@ -0,0 +1,17 @@
+package com.puente.springboot.demo.firstProjectSpringboot.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "HELLO world!" ;
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyWorkout() {
+        return "today is your lucky day.";
+    }
+}
