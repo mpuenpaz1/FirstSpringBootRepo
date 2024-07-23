e191a13967f147d14072a957290ede5aff44d82d
marcospuente <marcospuente24@gmail.com>
2024-07-24 01:49:35 +0200

    Spring Security and cmd run.

---
+++ 01-spring-boot-overview/03-actuator-demo/FirstSpringBoot-02/src/main/java/com/puente/springboot/demo/firstProjectSpringboot/FirstProjectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.puente.springboot.demo.firstProjectSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectSpringbootApplication.class, args);
+	}
+
+}
---
+++ 01-spring-boot-overview/05-command-line-demo/FirstSpringBoot-02/src/test/java/com/puente/springboot/demo/firstProjectSpringboot/FirstProjectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puente.springboot.demo.firstProjectSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
